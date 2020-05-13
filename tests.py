95d10229e4280ca9774032bebfb54e362cbf44ac
Joseph_Li <32281157+gak27ly@users.noreply.github.com>
2020-05-13 10:57:44 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,10 @@
 import unittest
-import task
-
 
 class TestCase(unittest.TestCase):
-
+    
     def test1(self):
         self.assertTrue(True)
 
-
-if __name__ == '__main__':
+        
+if __name__ == '__main':
     unittest.main()
